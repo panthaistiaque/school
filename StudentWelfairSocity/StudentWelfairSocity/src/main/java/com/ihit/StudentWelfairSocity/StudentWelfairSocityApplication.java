6997d49859728cc364a69e09b62213464f017ade
panthaistiaque <panthaistiaque@gmail.com>
2019-09-28 19:54:19 +0600

    657
    88

---
+++ StudentWelfairSocity/StudentWelfairSocity/src/main/java/com/ihit/StudentWelfairSocity/StudentWelfairSocityApplication.java
@@ -0,0 +1,13 @@
+package com.ihit.StudentWelfairSocity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentWelfairSocityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentWelfairSocityApplication.class, args);
+	}
+
+}
